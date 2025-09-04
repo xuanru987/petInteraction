1e3820706732eb4e0364dd811ee3577da109555c
Sydney <164105423+xuanru987@users.noreply.github.com>
2025-09-04 12:10:51 +1200

    Can check if it's alive and can show that it's dead

--- Pet.java
+++ Pet.java
@@ -16,49 +16,75 @@ public class Pet
     //Initialise instance variables
     private String type; //Type of animal
     private String name; //Name of pet
-    private double weight; //Weight of pet in Kg
+    private double weight; //Current weight of pet in Kg
+    private double equilibriumWeight; //Most suitable weight
     private double minWeight; //Minimum weight to stay alive
     private double maxWeight; //Maximum weight to stay alive
     private double metabolism; //Metabolism, in how many Kg it'll lose per hour without food (easier for the user)
     private int age; //Age of pet in yrs
-    private String image; //Image of head
+    private String image; //Image of pet
+    private String deadImage; //Image of pet when it dies
     private int imageX; //X-coordinate of image
     private int imageY; //Y-coordinate of image
     private int rightAttributes; //Right side of right column attributes 
+    
+    
+    private HashMap<String, Double>animalWeights; //Newborn weights of each animal type
     /**
      * Constructor for objects of class Pet
      */
-    public Pet(String type, String name)
+    public Pet(String type, String name, int age, double weight)
     {
         // initialise instance variables
         this.type = type;
         this.name = name;
-        this.age = 0;
+        this.age = age;
+        this.weight = weight;
         
         if (type.equals("dog")){
             image = "Dog.jpg";
-            weight = 0.2;
+            deadImage = "deadDog.jpg";
             imageX = 100;
             imageY = 100;
         }
         else if (type.equals("cat")){
             image = "Cat.jpg";
-            weight = 0.1;
+            deadImage = "deadCat.jpg";
             imageX = 100;
             imageY = 100;
         }
         else{
             image = "Otter.jpg";
-            weight = 1.0;
+            deadImage = "deadOtter.jpg";
             imageX = 80;
             imageY = 120;
         }
         
-        //Calculate other weight-related variables from the pet's weight
-        //MathGPT spotted that I put these lines above where this.weight was set and I moved them down (24/08/25)
-        maxWeight = weight * 2;
-        minWeight = weight/2;
-        metabolism = weight * 0.0005;
+        animalWeights = new HashMap<String, Double>();
+        animalWeights.put("dog", 0.2);
+        animalWeights.put("cat", 0.1);
+        animalWeights.put("otter", 1.0);
+        
+        setEquilibriumWeight(); //Calculate the most suitable weight from age and newborn weight
+        
+        //Calculate max and min weight from the pet's most suitable weight
+        maxWeight = equilibriumWeight * 2;
+        minWeight = equilibriumWeight/2;
+        
+        //Calculate metabolism from weight - MathGPT reminded me to put this below the code that sets the initial "weight" of the object;
+        metabolism = weight * 0.0005; 
+    }
+    
+    /**
+     * Calculate and set/update the most suitable weight for the pet using its age
+     */
+    public void setEquilibriumWeight(){
+        if(age <= 5){
+            this.equilibriumWeight = animalWeights.get(type) * (1 + age);
+        }
+        else{
+            this.equilibriumWeight = animalWeights.get(type) * 6;
+        }
     }
     
     /**
@@ -138,5 +164,33 @@ public class Pet
      */
     public void changeWeight(double ratio){
         this.weight = weight * (1 + ratio);
+        metabolism = this.weight * 0.0005; 
+    }
+    
+    /**
+     * Check if the pet is within the allowed weight range
+     * If not, the pet dies
+     * Return whether it is alive or not
+     * @return boolean petAlive - whether the pet is alive or not
+     */
+    public boolean checkAlive(){
+        boolean petAlive = true;
+        if(weight < minWeight || weight > maxWeight){
+            petAlive = false;
+        }
+        return petAlive;
+    }
+    
+    /**
+     * Change the image of the pet to show that it is dead
+     */
+    public void showDead(){
+        UI.drawImage(deadImage, imageX, imageY);
+        if(weight > maxWeight){
+            UI.drawString("Oops! " + name + " has died from being overweight.", 130, 70);
+        }
+        else if(weight < minWeight){
+            UI.drawString("Oops! " + name + " has died from being underweight.", 130, 70);
+        }
     }
 }

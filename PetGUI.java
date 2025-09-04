3124f79d0fb14f3f7736e572a207bcbcb43097eb
Sydney <164105423+xuanru987@users.noreply.github.com>
2025-09-04 12:09:34 +1200

    Can store pets when user quits; pet dies when outside weight range

--- PetGUI.java
+++ PetGUI.java
@@ -2,6 +2,7 @@
  * A GUI that interacts with the user to allow them to create a pet and take care of it
  * Displays a welcome message at first 
  * After pet selection the pet, its attributes, the player's resources and the pet's behaviour are displayed
+ * Sources: ChatGPT 2/9/25, 3/9/25
  * @author Sydney Liu
  * @version 26/07/2025
  */
@@ -9,6 +10,7 @@ import java.awt.Color;
 import java.awt.Font;
 import ecs100.*;
 import java.util.HashMap;
+import java.time.Instant; //Class to access current time in milliseconds since 12am 1/1/1970 GMT + 0
 
 public class PetGUI{
     private Pet pet; //The pet in the game
@@ -49,6 +51,13 @@ public class PetGUI{
     private Foods foods; //The collection of foods for the pet
     private Exercise exercise; //Exercise options for the pet
     
+    private HashMap<String, Double>animalWeights; //Initial weights of each animal type
+    
+    private ReadFromFile attributesReader; //Object to read the pet and UI's attributes that were stored in a file when the user last exited the program
+    private HashMap<Integer, String> previousAttributes; //Attributes of pet and UI from last time, read from file
+    private long lastTimeStamp; // The time when the user last exited the game (ms)
+    private long tSinceQuit; //Time since the user last quit in (h)
+     
     public PetGUI(){
         
         UI.initialise();
@@ -57,6 +66,7 @@ public class PetGUI{
         UI.addButton("Otter", this :: showOtter);
         UI.addButton("Foods", this :: showHideFoods);
         UI.addButton("Exercise", this :: showHideExercise);
+        UI.addButton("Save & exit", this :: saveNExit);
         UI.setMouseListener(this :: doMouse);
         
         leftAttributes = 20;
@@ -84,13 +94,39 @@ public class PetGUI{
         foodHeight = 50;
         foodGap = 50;
         foodDistance = foodWidth + foodGap;
+        
+        animalWeights = new HashMap<String, Double>();
+        animalWeights.put("dog", 0.2);
+        animalWeights.put("cat", 0.1);
+        animalWeights.put("otter", 1.0);
+        
+        attributesReader = new ReadFromFile();
+        previousAttributes = attributesReader.readAttributes();
+        existingPet();
+        
+    }
+    
+    /**
+     * Create the object of a pet that already existed last time the user used the program
+     * The pet's attributes were stored in a file
+     */
+    public void existingPet(){
+        if(previousAttributes.get(0).equals("true")){ //If the file says that there is an existing pet
+            lastTimeStamp = Long.valueOf(previousAttributes.get(5)); //Access the time when the user last quit
+            tSinceQuit = (Instant.now().toEpochMilli() - lastTimeStamp)/3600000; //time passed = current time - lastTimeStamp; then convert to h
+            createPet(previousAttributes.get(1), previousAttributes.get(2), Integer.valueOf(previousAttributes.get(3)), Double.valueOf(previousAttributes.get(4)));
+            petPresent = true;
+            pet.changeWeight(-tSinceQuit * pet.getMet()/pet.getWeight()); //Subtract the weight lost while the user was away -- chatGPT reminded me to parse in the subtracted amount as a ratio of the pet's weight and not a value
+        }
     }
     
     /**
      * Display welcome message
      */
     public void welcome(){
+        if(previousAttributes.get(0).equals("false")){
         UI.drawString("Welcome to Pet Interactor. Bring a pet home by clicking a button :)", 80, 50);
+        }
     }
     
     /**
@@ -133,13 +169,14 @@ public class PetGUI{
      * Create and show a certain pet and its attributes
      * @param String type - type of animal
      * @param String name - name of pet
-     * @param double weight - initial weight
+     * @param int age - current age
+     * @param double weight - current weight
      */
-    public void createPet(String type, String name){
+    public void createPet(String type, String name, int age, double weight){
         //MathGPT spotted that I had an "if petPresent == false condition", which would stop this method from being called after clicking "select", so I removed the condition
         //24/8/25
         UI.eraseString("Welcome to Pet Interactor. Bring a pet home by clicking a button :)", 80, 50);
-        this.pet = new Pet(type, name);
+        this.pet = new Pet(type, name, age, weight);
         pet.displayIm();
         showAttributes();
         foods = new Foods(pet, foodTop, foodLeft, foodWidth, foodHeight, foodGap);
@@ -191,19 +228,33 @@ public class PetGUI{
                 UI.eraseRect(selectLeft, selectTop, selectWidth, selectHeight); //Erase the "select" button
                 UI.drawString("What name would you like for your pet? Respond in the text panel on the left", 90, 60);
                 petName = UI.askString("Pet name: ");
-                createPet(previewedType, petName);
+                createPet(previewedType, petName, 0, animalWeights.get(previewedType));
                 UI.eraseString("What name would you like for your pet? Respond in the text panel on the left", 90, 60);
             }
-            if(foodsShown == true) {//MathGPT reminded me to put == and not = (29/8/25)
-                UI.eraseString("Current weight: " + pet.getWeight(), leftAttributes, topAttributes); //Erase the pet's weight
+            if(foodsShown == true) {//MathGPT reminded me to put == and not = (29/8/25) //This also means petPresent == true
+                UI.eraseString("Current weight: " + pet.getWeight() + " kg", leftAttributes, topAttributes); //Erase the pet's weight
                 foods.addWeight(x, y); //If the user clicks while the foods are shown, add an appropriate weight to the pet according to the food (or no food) clicked
-                UI.drawString("Current weight: " + pet.getWeight(), leftAttributes, topAttributes); //Re-display the pet's weight
+                UI.drawString("Current weight: " + pet.getWeight() + " kg", leftAttributes, topAttributes); //Re-display the pet's weight
+                checkDead();
             }
-            if(exerciseShown == true) {
+            else if(exerciseShown == true) { //This also means petPresent == true
                 UI.eraseString("Current weight: " + pet.getWeight() + " kg", leftAttributes, topAttributes); //Erase the pet's weight
                 exercise.subtractWeight(x, y); //If the user clicks while the exercise options are shown, subtract an appropriate weight from the pet according to the option (or no option) clicked
                 UI.drawString("Current weight: " + pet.getWeight() + " kg", leftAttributes, topAttributes); //Re-display the pet's weight
+                checkDead();
             }
+        }
+    }
+    
+    /**
+     * Check if the pet is dead, and if so, display screen for dead pet
+     */
+    public void checkDead(){
+        if(pet.checkAlive() == false){
+            UI.clearGraphics();
+            pet.showDead();
+            UI.drawString("Choose a new pet by clicking one of the buttons.", 80, 95);
+            petPresent = false;
         }
     }
     
@@ -258,11 +309,25 @@ public class PetGUI{
     }  
     
     /**
+     * Store the pet's information when the user exits the program
+     */
+    public void saveNExit(){
+        WriteToFile attributesWriter;
+        if(petPresent == true){
+            attributesWriter = new WriteToFile(petPresent, pet.getType(), pet.getName(), pet.getAge(), pet.getWeight(), Instant.now().toEpochMilli()); //Instant.now().toEpochMilli() -- current time in ms since 12am 1/1/1970
+        }
+        else{
+            attributesWriter = new WriteToFile(petPresent, "no type", "no name", 0, 0.0, Instant.now().toEpochMilli());
+        }
+        attributesWriter.writeToFile();
+        UI.quit();
+    }
+    
+    /**
      * Main method
      */
     public static void main(String [] args){
         PetGUI petGui = new PetGUI();
         petGui.welcome();
     }
-    
 }
